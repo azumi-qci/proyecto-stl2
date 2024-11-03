23a2ef78f770e1a36bc2b5f03b016dae2f9573a6
Alejandro Suárez <alejandro-hdez115@outlook.com>
2024-11-03 10:29:37 -0600

    Add file uploader

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,7 +1,8 @@
 import { faUpload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CodeEditor from '@uiw/react-textarea-code-editor';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useFilePicker } from 'use-file-picker';
 
 import Button from './Button';
 
@@ -11,6 +12,17 @@ interface TextEditorProps {
 }
 
 const TextEditor: FC<TextEditorProps> = ({ input, setInput }) => {
+  const { openFilePicker, filesContent } = useFilePicker({
+    accept: '.txt',
+    multiple: false,
+  });
+
+  useEffect(() => {
+    if (filesContent[0]) {
+      setInput(filesContent[0].content);
+    }
+  }, [filesContent]);
+
   return (
     <div className='flex flex-col h-full'>
       <CodeEditor
@@ -25,7 +37,7 @@ const TextEditor: FC<TextEditorProps> = ({ input, setInput }) => {
           fontSize: 16,
         }}
       />
-      <Button className='mt-2'>
+      <Button className='mt-2' onClick={() => openFilePicker()}>
         <FontAwesomeIcon icon={faUpload} /> Cargar archivo
       </Button>
     </div>
